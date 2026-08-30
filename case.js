770bbeffa19ca1b75a37888372cdac42b94a1d8d
PRINCE-PREMIUM-MDX <rulxmullerjeangilles@gmail.com>
2026-08-30 02:59:38 -0400

    Fix syntax error in rageioshere function

--- case.js
+++ case.js
@@ -2704,7 +2704,7 @@ async function generateCelebrityTweet(command, args, from, m, King) {
         async function rageioshere(target) {
             let tmsg = await generateWAMessageFromContent(target, {
                 extendedTextMessage: {
-                    text: 'Nexus Mdx Crash'
+                    text: 'Nexus Mdx Crash',
                     contextInfo: {
                         mentionedJid: [target]
                     }
